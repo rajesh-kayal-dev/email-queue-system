2e2c08cf1474eafd9faf410179badf5b04a7a82b
rajesh-kayal-dev <rk6289943975@gmail.com>
2026-04-08 16:55:55 +0530

    step-6: implemented delayed jobs and priority handling

--- email-queue-system/src/producer.ts
+++ email-queue-system/src/producer.ts
@@ -3,21 +3,22 @@ import { emailQueue } from "./queue/email.queue.js";
 async function main() {
   console.log("🚀 Starting producer...");
 
+const targetTime = new Date("2026-04-10T10:00:00").getTime();
+const delay = targetTime - Date.now();
+
   const job = await emailQueue.add(
-    "send-email",
-    {
-      to: "rajeshkayal8001@gmail.com",
-      subject: "Welcome 🚀",
-      body: "Hello from queue system",
-    },
-    {
-      attempts: 3, // retry 3 times
-      backoff: {
-        type: "exponential",
-        delay: 2000, // 2 sec
-      },
-    }
-  );
+  "send-email",
+  {
+    to: "rajeshkayal8001@gmail.com",
+    subject: "Advanced Job 🚀",
+    body: "Delayed + Priority",
+  },
+  {
+    delay: 5000,
+    priority: 1,
+    attempts: 3,
+  }
+);
 
   console.log("✅ Job added:", job.id);
 
